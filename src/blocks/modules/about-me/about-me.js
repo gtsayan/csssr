1cd31072c3c27752657eacbe60cb07d8e4e3cd4f
gtsayan <48355689+gtsayan@users.noreply.github.com>
2020-01-13 18:40:55 +0300

    js-level

---
+++ src/blocks/modules/about-me/about-me.js
@@ -0,0 +1,13 @@
+modules.define('about-me', ['i-bem-dom'], function(provide, bemDom) {
+
+provide(bemDom.declBlock(this.name, {
+    onSetMod: {
+        js: {
+            inited: function() {
+                
+            }
+        }
+    }
+}));
+
+});
